b0947b0f077e49b8154b49a77ed39c9629214c4f
Michael Rodríguez <92814886+Mik387@users.noreply.github.com>
2021-11-17 15:17:13 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8"> <!--Codificacion de caracteres, impedira que salgan caracteres raros en internet-->
+        <meta name="description" content="Coca Cola es la bebida numero 1 del mundo"> <!--Resumen en menos de 130 caracteres de que trata la web-->
+        <meta name="keywords" content="bebida, gaseosa, cola, refresco"> <!--palabras clave para posicionar la pagina en buscadres estas se ponen en el content--> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Inicio - Coca Cola
+        </title>
+        <link rel="shortcut icon" href="img/favicon.png">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/63ffb6097e.js" crossorigin="anonymous"></script>
+        </head>
+    <body>
+        <header>
+            <div class="container">
+                <a href="index.html">
+                    <img src="img/logo.svg" class="logo">
+                </a>
+                <nav>
+                    <a href="#inicio">Inicio</a>
+                    <a href="#nosotros">Nosotros</a>
+                    <a href="#servicios">Servicios</a>
+                    <a href="#galeria">Galería</a>
+                    <a href="#contactenos">Contáctenos</a>
+                </nav>
+                <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+            </div>
+        </header>
+        <main>
+            <section id="inicio">
+                <img src="img/bannerprincipal.jpg">
+                <div class="bloque-inicio">
+                    <h1>Bienvenidos a Coca Cola</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <a href="#nosotros" class="boton boton-rojo">Ver más</a>
+                </div>
+            </section>
+            <section id="nosotros" class="seccion">
+                <div class="container">
+                    <p>Maecenas magna elit, posuere a bibendum nec, tempus a turpis. Fusce vehicula viverra faucibus. Sed aliquam fringilla dignissim. Fusce mattis libero mauris, placerat iaculis metus vulputate nec. Donec justo ante, molestie et magna in, efficitur semper ligula.</p>   
+                </div>
+            </section>
+            <section id="servicios" class="seccion">
+                <div class="container">
+                    <div class="row">
+                        <div class="columna columna-33 columna-mobile-100" >
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto">
+                                    <img src="img/servicio1.jpg">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 1</h3>
+                                    <p>Maecenas magna elit, posuere a bibendum nec, tempus a turpis. Fusce vehicula viverra faucibus. Sed aliquam fringilla dignissim.</p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto" >
+                                    <img src="img/servicio2.jpg">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 2</h3>
+                                    <p>Maecenas magna elit, posuere a bibendum nec, tempus a turpis. Fusce vehicula viverra faucibus. Sed aliquam fringilla dignissim.</p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto">
+                                    <img src="img/servicio3.jpg">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 3</h3>
+                                    <p>Maecenas magna elit, posuere a bibendum nec, tempus a turpis. Fusce vehicula viverra faucibus. Sed aliquam fringilla dignissim.</p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="galeria">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg">
+                                <h4>Imagen1</h4>
+                            </div>
+                        </div>                        
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg">
+                                <h4>Imagen2</h4>
+                            </div> 
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg">
+                                <h4>Imagen3</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg">
+                                <h4>Imagen4</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg">
+                                <h4>Imagen5</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg">
+                                <h4>Imagen6</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="contactenos" class="seccion">
+                <iframe width="520" height="400" frameborder="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Puebla%20City+(Puebla)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+                            <form action="index.php" method="post">
+                                <div class="form-block">
+                                    <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                                </div>
+                                <div class="form-block">
+                                    <input type="email" name="email" class="form-control" placeholder="E-mail">
+                                </div>
+                                <div class="form-block">
+                                    <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                                </div>
+                                <div class="form-block bloque-ultimo">
+                                    <input type="submit" class="boton boton-negro" value="Enviar">
+                                </div>
+                                <?php
+
+                                    if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    {
+                                        $nombre = $_POST["nombre"] ;
+                                        $nombre = $_POST["email"] ;
+                                        $nombre = $_POST["mensaje"] ;
+
+                                        if(isset($nombre))
+                                        {
+                                            if(isset($email))
+                                            {
+                                                if(isset($mensaje))
+                                                {
+                                                    $para = "michael.extremebass@gmail.com";
+                                                    $asunto = "Esto es una prueba";
+                                                    $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                    $adicional = "From: michael_maro@hotmail.com";
+                                                    
+                                                    mail($para,$asunto,$cuerpo,$adicional);
+                                                ?>
+                                                
+                                                    <p>Envio Exitoso</p>
+                                                <?php
+                                                
+                                                }
+
+                                            }
+
+                                        }
+                                    }
+                                ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                 
+            </section>
+        </main>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-25 columna-mobile-100">
+                        <img src="img/logo-blanco.png" class="logo-footer">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies odio turpis, sed rhoncus libero auctor a.
+                        </p>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Temas Relacionados
+                        </h3>
+                        <ul>
+                            <li><a href="#">Tema 1</a></li>
+                            <li><a href="#">Tema 2</a></li>
+                            <li><a href="#">Tema 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Datos de Contacto
+                        </h3>
+                        <ul>
+                            <li>michael_maro@hotmail.com</li>
+                            <li>+52 123456789</li>
+                            <li>Av. Periferico 23503<br>Puebla, Pue México </li>
+                        </ul>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Redes Sociales
+                        </h3>
+                        <ul class="redes">
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-facebook-square"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-twitter-square"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-instagram-square"></i>
+                                </a>
+                            </li> 
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="barra-footer">
+                &copy; Derechos Reservados - 2021
+            </div>
+        </footer>
+        <script src="js/jquery.js"></script>
+        <script src="js/funciones.js"></script>
+    </body>
+</html>
